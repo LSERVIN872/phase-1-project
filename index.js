64b20b6c6fa1631ad261b126a96053479056706e
luis <servinluis21@yahoo.com>
2023-03-23 18:04:57 -0400

    complete

--- index.js
+++ index.js
@@ -35,4 +35,23 @@ function tileClick(event) {
     if (gameOverArea.classList.contains("visible")) {
       return;
     }
+    const tile = event.target;
+  const tileNumber = tile.dataset.index;
+  if (tile.innerText != "") {
+    return;
+  }
+  if (turn === PLAYER_X) {
+    tile.innerText = PLAYER_X;
+    boardState[tileNumber - 1] = PLAYER_X;
+    turn = PLAYER_O;
+  } else {
+    tile.innerText = PLAYER_O;
+    boardState[tileNumber - 1] = PLAYER_O;
+    turn = PLAYER_X;
+  }
+  setHoverText();
+  checkWinner();
+}
+
+
 
